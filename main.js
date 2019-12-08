d9169e31e9e2b6d8023574bcbdaf7fd9ff01479c
Manucheckhr Rakhimov <manucher5160@gmail.com>
2019-12-08 10:08:25 +0300

    updated main js and readme

--- main.js
+++ main.js
@@ -25,27 +25,28 @@ function init() {
         myMap.geoObjects.add(collection);
         // Добавляем подменю.
       
-          
-            submenu2.on('change',function()
+        submenu2.click();
+            submenu2.bind('click',function()
             {
+                
                // alert($(this).val());
                 for (var j = 0, m = group.items.length; j < m; j++) { //в случае изменения клика по метке изменить центр карты и клик по метке.
                     placemark = new ymaps.Placemark(group.items[j].center, { balloonContent: group.items[j].name });//добавить метки на карту. и название.
                     collection.add(placemark);
 
                     if($(this).val()==group.items[j].id)
-                        {
+                        { 
                             myMap.setCenter(group.items[j].center);//изменяет центр 
                             if (!placemark.balloon.isOpen()) { 
                                 placemark.balloon.open();
                             }
-            
+                            myMap.setCenter([53.896468, 27.546445]);
                             myMap.setZoom(19); //максимальное увеличение обзора местности
                         }
 
                 }
             });
-
+            
         for (var j = 0, m = group.items.length; j < m; j++) {
             createSubMenu(group.items[j], collection, submenu2);
         }
